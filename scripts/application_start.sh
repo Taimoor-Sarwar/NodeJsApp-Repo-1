bccfc918e4185178ef00a1419e6aa941e290f9fe
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-13 14:59:30 +0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-cd /home/ec2-user
-sudo pm2 restart nodejs-express-app
+sudo pm2 restart nodejs-express-app >> /home/ec2-user/
