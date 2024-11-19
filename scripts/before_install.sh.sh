ebb5801a4da4e3c059494f449df38206315f9528
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-19 11:02:44 +0500

    Add files via upload

---
+++ scripts/before_install.sh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'run application_start.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
+
+echo 'pm2 restart nodejs-express-app' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
+pm2 restart nodejs-express-app >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
