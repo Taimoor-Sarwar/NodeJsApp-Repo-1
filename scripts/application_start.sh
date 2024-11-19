bf38f2e1987668de5aaeefc0194e6a92d0e825c4
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-19 11:49:55 +0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
 
 cd /home/ubuntu/NodeJsApp-Repo-1
-echo 'pm2 restart nodejs-express-app' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
+echo 'pm2 restart app.js' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
 pm2 restart app.js >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
