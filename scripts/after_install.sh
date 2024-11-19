9afbf96df6874956fc61cf31ed73fb298c764aad
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-19 11:44:34 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
+echo 'cd /home/ubuntu/NodeJsApp-Repo-1' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
 cd /home/ubuntu/NodeJsApp-Repo-1 >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
 
 echo 'npm install' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log 
