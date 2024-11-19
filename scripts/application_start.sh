e9e7c126a53d4bc83806f0445006bc575735de96
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-19 10:52:29 +0500

    Delete scripts/application_start.sh

--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-
-echo 'pm2 restart nodejs-express-app' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-pm2 restart nodejs-express-app >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
