9f01ffa985ba5527cde6f7a3ca3cace2c297b37a
Taimoor-Sarwar <161565989+Taimoor-Sarwar@users.noreply.github.com>
2024-11-19 11:06:10 +0500

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-
-echo 'cd /home/ubuntu/NodeJsApp-Repo-1' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-cd /home/ubuntu/NodeJsApp-Repo-1 >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-
-echo 'npm install' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log 
-npm install >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
--- scripts/application_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-
-cd /home/ubuntu/NodeJsApp-Repo-1
-echo 'pm2 restart app.js' >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
-pm2 restart app.js >> /home/ubuntu/NodeJsApp-Repo-1/deploy.log
